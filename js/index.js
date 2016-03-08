6cfc07df56821f4c55cfac9f1447ad9b579c83e8
cckkll <237178842@qq.com>
2016-03-08 12:08:44 +0800

    delete drawsvg and add vivus

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ function fetch_data(){
   generate_record()
   var data = $("#signature").jSignature("getData", "svg");
   var paths = $(data[1]).find("path")
+  $('#signature').jSignature('reset');
   $('#svg').empty().append(paths);
   new Vivus('svg', {type: 'oneByOne',duration: 500});
+
 }
 
 function generate_record(){
